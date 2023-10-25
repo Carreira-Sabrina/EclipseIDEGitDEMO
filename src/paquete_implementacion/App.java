3a95c14a699c1ee1baefd99f7acb9a54bc014766
Carreira-Sabrina <126538635+Carreira-Sabrina@users.noreply.github.com>
2023-10-25 04:57:58 -0300

    Ahora se invoca el metodo sumar con un print re cheto

--- src/paquete_implementacion/App.java
+++ src/paquete_implementacion/App.java
@@ -4,10 +4,13 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int resultadoSuma = sumar(10,59);
+		String mensajeResultado = "El resultado de la suma es " + resultadoSuma;
+		System.out.println(mensajeResultado);
 
 	}
 	
-	public int sumar(int a, int b) {
+	public static int sumar(int a, int b) {
 		return a + b;
 	}
 
