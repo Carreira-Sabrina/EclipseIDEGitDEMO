eab08483a5131fe593bfdaaebce79fbd5b5a65ea
Carreira-Sabrina <126538635+Carreira-Sabrina@users.noreply.github.com>
2023-10-25 04:52:55 -0300

    Este es el primer intento de meter algo al Git desde Eclipse que me parece el IDE mas horrendo del pueblo

---
+++ src/paquete_implementacion/App.java
@@ -0,0 +1,14 @@
+package paquete_implementacion;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int sumar(int a, int b) {
+		return a + b;
+	}
+
+}
